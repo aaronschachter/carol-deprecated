4157d11806aa14465bca198375339fb5e36fad07
Aaron Schachter <aaronschachter@gmail.com>
2016-03-29 15:19:40 -0700

    Moves js to src, adds placeholder login form

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ function handleErrors() {
 
 function buildScript(file, watch) {
   var props = {
-    entries: ['./app/' + file],
+    entries: ['./src/' + file],
     debug : true,
     cache: {},
     packageCache: {},
@@ -40,22 +40,15 @@ function buildScript(file, watch) {
       .pipe(gulp.dest('./public/dist/'))
       .pipe(reload({stream:true}))
   }
-
-  // listen for an update and run rebundle
   bundler.on('update', function() {
     rebundle();
     gutil.log('Rebundle...');
   });
 
-  // run it once the first time buildScript is called
   return rebundle();
 }
 
-gulp.task('scripts', function() {
-  return buildScript('campaigns.js', false); // this will run once because we set watch to false
+gulp.task('default', function() {
+  return buildScript('campaigns.js', true); 
 });
 
-// run 'scripts' task first, then watch for future changes
-gulp.task('default', ['scripts'], function() {
-  return buildScript('campaigns.js', true); // browserify watch for JS changes
-});
