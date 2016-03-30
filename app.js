0ad77409b0c9a336453985aeaffa98e3cddc9e32
Aaron Schachter <aaronschachter@gmail.com>
2016-03-29 22:02:23 -0700

    Fix up homepage, /campaigns routes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
-var router = express.Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +18,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Login' });
+});
+
+app.get('/campaigns', function(req, res, next) {
+  res.render('campaigns', { title: 'Login' });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -51,8 +58,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Campaigns' });
-});
+
+// app.get('/campaigns', function(req, res, next) {
+//   res.render('campaigns', { title: 'Campaigns' });
+// });
 
 module.exports = app;
